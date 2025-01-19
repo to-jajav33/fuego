6825975a190d84884d795a329185059f32b4fe60
Javier Mendoza <jmendoza@mindresearch.org>
2025-01-18 19:35:27 -0800

    Use firetrucks larger area for detecting when fire alarm should go off

--- main/components/fire_station.gd
+++ main/components/fire_station.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 
-func _on_body_entered(body):
-	if (body.is_in_group("group_body_firetruck")):
+func _on_area_entered(area):
+	if (area.is_in_group("group_area_firetruck")):
 		if (GameState.currState != GameState.STATES_AVAILABLE.PLAY_FIRE_ALARM):
 			GameState.createFireAlarm();
 	pass # Replace with function body.
